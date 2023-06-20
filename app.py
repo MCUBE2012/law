2df844aae2d4c2288f1d44bc8c558bbbe022a955
MCUBE2012 <muhdmaryammusa@gmail.com>
2023-06-20 12:34:59 +0100

    updating flask

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,
+
+from flask_sqlalchemy import flask_sqlalchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+db = SQLAlchemy(app)
+
+class Contact(db.Model):
+    id= db.column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    subject = db.Column(db.String(200))
+    email = db.Column(db.String(200))
 
 
 @app.route('/')
@@ -9,6 +21,18 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/thankyou', methods =['POST'])
+def thankyou():
+    name = request.form['name']
+    email = request.form['email']
+    subject = request.form ['subject']
+    message = request.form['message']
+
+    data = Contact (name=name,email=email,subject=subject,message='message')
+    db.session.add(data)
+    db.session.commit()
+    return redirect (url_fro('home'))
 
 @app.route('/contact')
 def contact():
